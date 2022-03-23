a3cbd800a24c29252597f6098e28ef48b9cee7ba
Hanna Bina Nilsen <bina.99@live.se>
2022-03-23 17:47:09 +0100

    added functions renderStudents
    also added function to calculate totalCredits and render courses

--- students.js
+++ students.js
@@ -1,1 +1,54 @@
 "use strict";
+
+let students = DATABASE.students
+let student = [DATABASE.students[0], DATABASE.students[1]];
+
+function renderStudent (id){
+    let div = document.createElement("div");
+    let student = DATABASE.students[id];
+    div.id = "container";
+    div.innerHTML = `
+    <header> ${student.firstName} ${student.lastName} (total credits: ${totalCredits(student)})</header>
+    <div>
+        <div id="course">
+            <h3>Courses: </h3>
+            <div id="courses">
+                ${renderCourses(student)}
+            </div> 
+        </div>
+    </div>`
+    return div;
+}
+
+function totalCredits(student){
+    let credits = [];
+    for (let course of student.courses){
+        credits.push(course.passedCredits)
+    }
+    let creditsSum = 0;
+    for (let i = 0; i < credits.length; i++){
+        creditsSum += credits[i];
+    }
+    return creditsSum;
+}
+
+function renderStudents (students){
+    let studentsElement = document.getElementById("result");
+    for ( let student of students ) {
+        let studentElement = renderStudent(student.studentID);
+        studentsElement.appendChild(studentElement);
+    }
+}
+
+function renderCourses(student){
+    let courseData = DATABASE.courses;
+    let courses = [];
+    for (let course of student.courses) {
+        if (course.courseID == courseData.courseID){
+            courses.push(courseData);
+        }
+    }
+}
+
+
+renderStudents(DATABASE.students);
